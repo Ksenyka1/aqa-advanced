2a15afb5d1ee7b7583753ed759c0ff2c3105c40e
Oksana <oksanarevakina1@gmail.com>
2025-07-12 19:48:18 +0200

    Додано task1 з примітивними типами

---
+++ jsBasics/task1.js
@@ -0,0 +1,11 @@
+let myString = "Good morning";
+let myNumber = 10;
+let myBoolean = true;
+let myNull = null;               
+let myUndefined = undefined;
+
+console.log("Text:", myString);
+console.log("Number:", myNumber);
+console.log("Boolean:", myBoolean);
+console.log("Null:", myNull);
+console.log("Undefined:", myUndefined);
