f40d9d11914edb728d01755556013fb3c7898c25
Oksana <oksanarevakina1@gmail.com>
2025-07-13 13:12:13 +0200

    Add jsBasics folder

---
+++ jsBasics/task2.js
@@ -0,0 +1,8 @@
+const person1 = "Mark";
+const person2 = "Petro";
+
+const greetingConcat = "Hello, " + person1 + " and " + person2 + "!";
+console.log(greetingConcat);
+
+const greetingTemplate = `Hello, ${person1} and ${person2}!`;
+console.log(greetingTemplate);
---
+++ jsBasics/task4.js
@@ -0,0 +1,16 @@
+//Task1
+const radius = 5;
+const circleArea = Math.PI * radius ** 2;
+console.log("Circle area:", circleArea.toFixed(2));
+
+//Task2
+const length = 10;
+const width = 5;
+const rectangleArea = length * width;
+console.log("Rectangle area:", rectangleArea.toFixed(2));
+
+//Task3
+const cilinderRadius = 5;
+const height = 10;
+const cilinderVolume = Math.PI * cilinderRadius ** 2 * height;
+console.log("Cilinder volume:",cilinderVolume.toFixed(2));
